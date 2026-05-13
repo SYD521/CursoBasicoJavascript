76e756961df60b9190420358bc0b213dc6873127
Jose Daniel Luzuriaga <jdluva.jdl@gmail.com>
2026-05-13 18:48:43 -0500

    feat clase2: conocer diferencias variables var, let, y constante const

---
+++ 1_Primeros pasos/variables.js
@@ -0,0 +1,17 @@
+// var, variables globales
+var saludo = 'Hola CIAPFA'
+if (saludo) {
+    var saludo = 'HOLA'
+    console.log(saludo)
+}
+console.log(saludo)
+// let, variables limitadas al alcance del bloque donde las definimos
+let nombre = 'CIAPFA'
+if (nombre) {
+    let nombre = 'Alejo'
+    console.log(nombre)
+}
+console.log(nombre)
+// const, variables inmutables
+const PI = 3.14
+console.log(PI)
