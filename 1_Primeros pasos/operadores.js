07a4eaeff89cce94d826115a3c4311063b08f44f
JD Mac <jdluva.jdl@gmail.com>
2026-05-14 12:03:40 -0500

    feat clase5: conocer los operadores mas basicos

---
+++ 1_Primeros pasos/operadores.js
@@ -0,0 +1,45 @@
+// Operadores aritméticos +, -, *, /, %, **, ++, --
+
+var a = 2
+//a = a + 3 //suma
+//a = a - 3 //resta
+//a = a * 3 //multiplicacion
+//a = a / 3 //division
+//a = a % 3 //resto (residuo division)
+a = a ** 3 //potenciacion
+
+console.log(a)
+
+// Operadores de asignación =, +=, -=, *=, /=, %=, **=
+
+var b = 3
+//b += 3 // b = b + 3
+//b -= 3 // b = b - 3
+//b *= 3 // b = b * 3
+//b /= 3 // b = b / 3
+//b %= 3 // b = b % 3
+b **= 3 // b = b ** 3
+
+console.log(b)
+
+// Operadores de comparación ==, !=, ===, !==, >, >=, <, <=
+
+console.log(a == b) //devuleve true o false dependiendo de la comparación
+
+// Operadores lógicos && (AND), || (OR), ! (NOT)
+
+var v = true
+var f = false
+
+console.log(v && f)
+
+// Operador condicional (ternario) condicion ? true: false
+
+var edad = 19
+var res = edad >= 18 ? "Es mayor de edad": "Es menor de edad"
+
+console.log(res)
+
+
+
+
