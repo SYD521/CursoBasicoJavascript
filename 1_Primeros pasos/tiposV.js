00f63b41977da0cc1f9d401faee2a17869d2222d
Jose Daniel Luzuriaga <jdluva.jdl@gmail.com>
2026-05-13 19:05:25 -0500

    feat clase3: conocer tipos de variables

---
+++ 1_Primeros pasos/tiposV.js
@@ -0,0 +1,21 @@
+var nombre = "Alejo" //string
+console.log(nombre) //number
+var miNumero = 50
+console.log(miNumero)
+const PI2 = 3.14 //decimal
+console.log(PI2)
+var verdadero = true //boolean true
+var falso = false //boolean false
+console.log(verdadero)
+console.log(falso)
+var nulo = null //variable null
+console.log(nulo)
+var indefinido = undefined //variable undefined
+console.log(indefinido)
+
+// Para saber que tipo de variable tenemos
+console.log(typeof verdadero);
+
+// constantes aritmeticas en Javascript
+const PI = Math.PI
+console.log(PI)
