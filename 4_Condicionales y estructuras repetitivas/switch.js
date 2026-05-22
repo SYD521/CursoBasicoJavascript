068e7d4b6c1a6842d035f401093169c83cb277a9
SamaraPuga <sammypuga87@outlook.com>
2026-05-22 10:17:18 -0500

    feat clase13: conocer SWITCH

---
+++ 4_Condicionales y estructuras repetitivas/switch.js
@@ -0,0 +1,22 @@
+// Recibe una expresion, y de acuerdo a ella ejecuta un caso
+
+var opcion = 1
+
+switch (opcion) {
+    case 1:
+        console.log("Menu de usuario")
+        break;
+    case 2:
+        console.log("Menu administrativo")
+        break;
+    case 3:
+        console.log("Configuración")
+        break;
+    case "Nuevo menu":
+        let miNuevoMenu = "Salida"
+        console.log(miNuevoMenu)
+        break;
+    default:
+        console.log("Break")
+        break;
+}
